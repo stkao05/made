594416f8aec72b245a796f9c50e5f4ebfc5422ee
Steven Kao <st.kao.05@gmail.com>
2024-06-25 23:11:42 +0800

    use torch loss

--- train.py
+++ train.py
@@ -7,7 +7,6 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from made import MADE
 
-
 class MINST(Dataset):
   def __init__(self, data):
     self.data = data
@@ -19,17 +18,6 @@ class MINST(Dataset):
     return len(self.data)
 
 
-def nll(x, p):
-  return -1 * ((x * p.log()) + (1 - x) * (1 - p).log()).sum(dim=1)
-
-
-def nll_loss(x, p, smoothing=None):
-  if smoothing:
-    x = x * (1 - smoothing) + (1 - x) * smoothing
-
-  return -1 * ((x * p.log()) + (1 - x) * (1 - p).log()).sum(dim=1).mean()
-
-
 @torch.no_grad()
 def eval_split(ds, model, device):
   dl = DataLoader(ds, batch_size=128)
@@ -38,9 +26,7 @@ def eval_split(ds, model, device):
   for x in dl:
     x = x.to(device=device)
     logits = model(x)
-    p = F.sigmoid(logits)
-
-    loss = nll_loss(x, p)
+    loss = F.binary_cross_entropy_with_logits(logits, x, reduction="none").sum(dim=1).mean()
     lossi.append(loss.item())
 
   return torch.tensor(lossi).mean().item()
@@ -56,17 +42,14 @@ def train_loop(model, optim, train_ds, valid_ds, epoch, device):
       x = x.to(device=device)
       optim.zero_grad()
       logits = model(x)
-      p = F.sigmoid(logits)
-
-      loss = nll_loss(x, p, smoothing=0.005)
+      loss = F.binary_cross_entropy_with_logits(logits, x, reduction="none").sum(dim=1).mean()
       loss.backward()
-      norm = nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
       optim.step()
 
     tr_loss = eval_split(train_ds, model, device)
     va_loss = eval_split(valid_ds, model, device)
     end = time.time()
-    print(f"epoch {i+1} | train: {tr_loss:.2f} | valid: {va_loss:.2f} | norm: {norm:.2f} | time: {end - start:.2f}")
+    print(f"epoch {i+1} | train: {tr_loss:.2f} | valid: {va_loss:.2f} | time: {end - start:.2f}")
 
 
 def parse_int_list(s):
@@ -78,7 +61,7 @@ def parse_int_list(s):
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('-s', type=parse_int_list, help='hidden size, common separated')
+  parser.add_argument('-s', type=parse_int_list, help='hidden size, common separated', required=True)
   parser.add_argument('-e', type=int, help='epoch', default=5)
   parser.add_argument('-f', help='output path of the model weight', default=".data/mnist_made.py")
 
@@ -92,7 +75,9 @@ def main():
     valid_ds = MINST(torch.from_numpy(f["valid_data"]).float())
 
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-  model = MADE(input_size=784, hidden_sizes=hidden_sizes, output_size=784)
+  model = MADE(784, hidden_sizes, 784)
+  print("number of model parameters:",sum([np.prod(p.size()) for p in model.parameters()]))
+
   model.to(device=device)
   optim = torch.optim.Adam(model.parameters())
 
